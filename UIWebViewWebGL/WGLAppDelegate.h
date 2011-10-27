adc8613c003c5602379e3c81e0c7f11d4d2d6e5c
Nathan de Vries <nathan@atnan.com>
2011-10-27 17:18:58 +1100

    Initial Commit

---
+++ UIWebViewWebGL/WGLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WGLAppDelegate.h
+//  UIWebViewWebGL
+//
+//  Created by Nathan de Vries on 27/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WGLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UIWebViewWebGL/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UIWebViewWebGL
+//
+//  Created by Nathan de Vries on 27/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WGLAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WGLAppDelegate class]));
+    }
+}
