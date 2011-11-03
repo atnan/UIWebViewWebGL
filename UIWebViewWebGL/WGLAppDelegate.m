a66aa5546cd81deaa1103b699169d86601c061be
Nathan de Vries <nathan@atnan.com>
2011-11-03 12:54:10 +1100

    Working WebGL demo using standard UIWebViews.

--- UIWebViewWebGL/WGLAppDelegate.m
+++ UIWebViewWebGL/WGLAppDelegate.m
@@ -3,67 +3,39 @@
 //  UIWebViewWebGL
 //
 //  Created by Nathan de Vries on 27/10/11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2011 Nathan de Vries. All rights reserved.
 //
 
 #import "WGLAppDelegate.h"
 
 @implementation WGLAppDelegate
 
-@synthesize window = _window;
+@synthesize window=_window;
 
-- (void)dealloc
-{
-    [_window release];
-    [super dealloc];
-}
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-    self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
-    // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
-    [self.window makeKeyAndVisible];
-    return YES;
-}
-
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-    /*
-     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-     */
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    /*
-     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-     If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-     */
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    /*
-     Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-     */
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    /*
-     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-     */
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-    /*
-     Called when the application is about to terminate.
-     Save data if appropriate.
-     See also applicationDidEnterBackground:.
-     */
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
+  
+//  Class ADWebView = NSClassFromString(@"ADWebView");
+//  UIWebView* webView = (UIWebView *)[[[ADWebView alloc] initWithFrame:self.window.bounds] autorelease];
+//  [webView setWebGLEnabled:YES];
+    
+  UIWebView* webView = [[[UIWebView alloc] initWithFrame:self.window.bounds] autorelease];
+  
+  id webDocumentView = [webView performSelector:@selector(_browserView)];
+  id backingWebView = [webDocumentView performSelector:@selector(webView)];
+  [backingWebView _setWebGLEnabled:YES];
+  
+  NSString* htmlPath = [[NSBundle mainBundle] pathForResource:@"WebGL" ofType:@"html"];
+  NSURLRequest* request = [NSURLRequest requestWithURL:[NSURL fileURLWithPath:htmlPath]];
+  [webView loadRequest:request];
+  
+  UIViewController* viewController = [[[UIViewController alloc] init] autorelease];
+  viewController.view = webView;
+  
+  self.window.rootViewController = viewController;
+  [self.window makeKeyAndVisible];
+  
+  return YES;
 }
 
 @end
--- UIWebViewWebGL/main.m
+++ UIWebViewWebGL/main.m
@@ -3,7 +3,7 @@
 //  UIWebViewWebGL
 //
 //  Created by Nathan de Vries on 27/10/11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2011 Nathan de Vries. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
